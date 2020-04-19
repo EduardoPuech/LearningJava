cbdd8135384ea373126ed28c7f0464b41dbfa867
Eduardo Puech <eduardo.puech@gmail.com>
2020-04-19 13:30:10 +0200

    Update EmpleadoUnaFuente.java
    terminado

--- AprendiendoJava/src/empezamosPOO/EmpleadoUnaFuente.java
+++ AprendiendoJava/src/empezamosPOO/EmpleadoUnaFuente.java
@@ -6,6 +6,25 @@ public class EmpleadoUnaFuente { // todo en un uunico fichero fuente para probar
 
 	public static void main(String[] args) {
 
+		// instancio tres objetos de la clase empleado
+		Empleado empleado1 = new Empleado("Cesar de Echagüe", 25000, 2010, 03, 16); // enero es 00 pero le hemos dado
+																					// mes-1
+																					// luego esto seriia febrero.
+		Empleado empleado2 = new Empleado("Leonor de Acevedo", 24000, 2008, 01, 16);
+		Empleado empleado3 = new Empleado("Lupe de Torres", 26000, 2012, 02, 17);
+
+		// utilizo el meetodo subir el sueldo
+		empleado1.subidaSueldo(10);
+		empleado2.subidaSueldo(15);
+		empleado3.subidaSueldo(5);
+
+		System.out.println("Nombre: " + empleado1.getNombre() + "\nSueldo actual: " + empleado1.getSueldo()
+				+ "\nAnno de entrada en la empresa: " + empleado1.getAltaContrato());
+		System.out.println("Nombre: " + empleado2.getNombre() + "\nSueldo actual: " + empleado2.getSueldo()
+		+ "\nAnno de entrada en la empresa: " + empleado2.getAltaContrato());
+		System.out.println("Nombre: " + empleado3.getNombre() + "\nSueldo actual: " + empleado3.getSueldo()
+		+ "\nAnno de entrada en la empresa: " + empleado3.getAltaContrato());
+
 	}
 
 }
@@ -35,8 +54,9 @@ class Empleado { // creo a los empleados
 
 	// meetodo de tipo setter para el incremento de sueldo en funcioon del tiempo
 	// trabajado
-	public void setSubidaSueldo(double porcentaje) {
-		double aumento = sueldo*porcentaje/100;
+	public void subidaSueldo(double porcentaje) {
+		double aumento = sueldo * porcentaje / 100;
+		sueldo = sueldo + aumento;
 	}
 
 	// voy a crear las variables de clase al final para que se vea que se puede
