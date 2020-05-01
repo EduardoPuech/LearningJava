21dcd1b77d0349e65776876624c967a0421e7f77
Eduardo Puech <eduardo.puech@gmail.com>
2020-05-01 11:32:44 +0200

    Update CapturaDeFuentes.java
    cambiado para darle un input para que busque en la lista

--- AprendiendoJava/src/graficoSwing/CapturaDeFuentes.java
+++ AprendiendoJava/src/graficoSwing/CapturaDeFuentes.java
@@ -2,6 +2,8 @@ package graficoSwing;
 
 import java.awt.GraphicsEnvironment;
 
+import javax.swing.*;
+
 public class CapturaDeFuentes {
 	/*
 	 * TODO: este programa es una ayuda que me va a servir para saber quee tipos de
@@ -9,10 +11,21 @@ public class CapturaDeFuentes {
 	 * la clase GraphicsEnvironment
 	 */
 	public static void main(String[] args) {
+
+		String fuente = JOptionPane.showInputDialog("Introduce fuente");
+		boolean comprueboFuente = false;
 		String[] nombresDeFuentes = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-		
+
 		for (String i : nombresDeFuentes) {
-			System.out.println("Fuente: " + i);
+			if (i.equalsIgnoreCase(fuente)) {
+				comprueboFuente = true;
+			}
+		}
+		
+		if (comprueboFuente) {
+			System.out.println("Fuente instalada");
+		} else {
+			System.out.println("No estaa instalada dicha fuente");
 		}
 	}
 }
